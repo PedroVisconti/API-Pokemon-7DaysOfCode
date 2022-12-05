734a30350218f029726297a0b1222688a46dae25
PedroVisconti <pedroarthurvisconti@hotmail.com>
2022-12-05 10:18:24 -0300

    Adicionar arquivos de projeto.

---
+++ API Pokemon/BuscarPokemon.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API_Pokemon
+{
+    public class buscarPokemon {
+        public static Pokemon BuscarPokemon(string nomePokemon)
+        {
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{nomePokemon}");
+
+            var request = new RestRequest("",Method.Get);
+
+            var response = client.Execute(request);
+
+            return JsonSerializer.Deserialize<Pokemon>(response.Content);
+
+
+        }
+    }
+    
+}
---
+++ API Pokemon/Program.cs
@@ -0,0 +1,76 @@
+﻿using API_Pokemon;
+using RestSharp;
+using System.Runtime.CompilerServices;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+
+        List<Pokemon> pokemonAdotado = new List<Pokemon>();
+        string opcao;
+
+        do
+        {
+            Console.WriteLine("Bem vindo ao sistema Pokemon, oque deseja fazer hoje:" +
+                "\n 1 - Adotar um Pokemon;" +
+                "\n 9 - Sair do Sistema");
+            opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Pokemon pokemon = new();
+                    Console.WriteLine("Qual Pokemon deseja adotar:" +
+                        "\n Bulbasaur;" +
+                        "\n Squirtle" +
+                        "\n Charmander\n");
+
+                    string escolhaPokemonUsuario = Console.ReadLine();
+
+                    while (opcao != "3")
+                    {
+                        Console.WriteLine($"1 - Saber mais sobre {escolhaPokemonUsuario}" +
+                            $"\n2 - Adotar {escolhaPokemonUsuario}" +
+                            $"\n3 - Voltar\n");
+
+                        string opcao2 = Console.ReadLine();
+
+                        switch (opcao2) {
+
+                            case "1":
+                                pokemon = buscarPokemon.BuscarPokemon(escolhaPokemonUsuario);
+                                Console.WriteLine($"Informações sobre {escolhaPokemonUsuario}");
+                                Console.WriteLine($"Nome do Pokemon: " + pokemon.name);
+                                Console.WriteLine("Altura: " + pokemon.height);
+                                Console.WriteLine("Peso: " + pokemon.weight);
+
+                                Console.WriteLine("Habilidades: ");
+                                foreach (Abilities habilidade in pokemon.abilities)
+                                {
+                                    Console.Write(habilidade.ability.name.ToUpper() + " ");
+                                }
+                                Console.WriteLine();
+                                break;
+                        }
+                    }
+                    break;
+
+                case "9":
+                    Console.WriteLine("Obrigado por usar o sistema, até mais");
+                    break;
+            }
+
+
+
+        } while (opcao == "9");
+
+
+
+
+    }
+
+    
+}
+
+
